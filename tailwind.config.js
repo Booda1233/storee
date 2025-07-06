92ff1a84427827e5a8d4d67cb4a217ac57a74e3f
Booda1233 <149318977+Booda1233@users.noreply.github.com>
2025-07-06 22:59:29 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // ← عدّل حسب مساراتك
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
