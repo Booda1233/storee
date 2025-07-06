5a817372d604d0530a54edfbf9b2e5ae5e9feab5
Booda1233 <149318977+Booda1233@users.noreply.github.com>
2025-07-06 22:57:11 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
+import './index.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
